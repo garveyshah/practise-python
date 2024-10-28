c65fc0b4537845a693452813f5df1da7986c1277
oumouma <garveyshah1595@gmail.com>
2024-10-29 00:03:35 +0300

    variable

--- programiz/basics/variable-and-literals/variables.py
+++ programiz/basics/variable-and-literals/variables.py
@@ -10,4 +10,25 @@ print(site_name)
 
 # # Python is an `type-inferred` language, so you don't have to explicitly define the variable type.
 
-# # It automat
+# Changing the Value of a Variable 
+# # Assigning a new value to site_name
+site_name = 'apple.com'
+
+print(site_name)
+
+# # Assigning multiple values to multiplw variables
+a, b, c = 5, 3.2, 'Hello'
+
+print (a)
+print (b)
+print (c)
+
+# If we want to assign the same value to multiple variables at once, we can do this as:
+site1 = site2 = 'programiz.com'
+
+print (site1)
+print (site2)
+
+## Python Literals 
+# Literals are represtations of fixed values in a program.
+# They can 
