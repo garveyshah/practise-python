e24189ec1fa72f65605c2c3cceda25aa7a5431a8
oumouma <garveyshah1595@gmail.com>
2024-10-28 21:15:07 +0300

    Learning Python from Programmiz

---
+++ programiz/basics/comments/comments.py
@@ -0,0 +1,23 @@
+# Comments are hints that  we add to our code to make it easier to understand.
+# Python comments start with `#`. 
+
+# For example :
+
+# print a number
+print(25)
+
+# Here, `# print a number` is a comment.
+# Comments are completely ignored and not executed by code editors.
+
+# Single-Line Comment
+
+# We use the hash (`#`) symbol to write a single-line comment.
+# 
+#
+# # declare a variale
+#  name = "John"
+# 
+# # print name
+# print(name)   # John
+# 
+# In the above example, we have  
---
+++ programiz/basics/variable-and-literals/variables.py
@@ -0,0 +1,13 @@
+# Python Variables
+# A variable is a container (storage area) to hold data.
+number = 10
+
+# Assigning values to Variables
+# # assign value to site_name variable
+site_name = 'programiz.pro'
+
+print(site_name)
+
+# # Python is an `type-inferred` language, so you don't have to explicitly define the variable type.
+
+# # It automat
