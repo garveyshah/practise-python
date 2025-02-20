ff76bd15d1b63367897cad7a0d14d11b458456bd
oumaoumag <ouma.godwin10@gmail.com>
2025-02-20 18:21:43 +0300

    practise how to add, update, and delete records from the database practise how to display records in html

--- django/w3schools/my_tennis_club/members/views.py
+++ django/w3schools/my_tennis_club/members/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import Member
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
 
 def myworld(request):
     return HttpResponse("This is my World !")
 
 
 
-def members(request):
-    return HttpResponse("Hello world!")
+# def members(request):
+#     template = loader.get_template('myfirst.html')
+#     return HttpResponse(template.render())
 
